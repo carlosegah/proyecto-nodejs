3a7b6c130868802ec530b63b954aa2ab8837794d
Carlos Gallardo <c4rlosgallardo@gmail.com>
2025-12-15 23:57:14 -0300

    Added: NotFound and Middlewares

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from 'express';
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("¡Bienvenido a mi API REST!")
+
+app.use((req, res, next) => {
+    console.log(req.method);
+    next();
 });
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Bienvenidos a mi API REST"
+    });
+});
+
+import errores from './src/middlewares/not-found.js';
+app.use(errores);
 
 const PORT = 3000;
 
